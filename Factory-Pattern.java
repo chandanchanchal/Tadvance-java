4a6ef92a8f67b0531d8525129a6203cf48d2d406
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2022-11-10 09:59:25 +0530

    Add files via upload

---
+++ Factory-Pattern.java
@@ -0,0 +1,55 @@
+public interface Notification {
+	void notifyUser();
+}
+public class SMSNotification implements Notification {
+
+	@Override
+	public void notifyUser()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Sending an SMS notification");
+	}
+}
+public class EmailNotification implements Notification {
+
+	@Override
+	public void notifyUser()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Sending an e-mail notification");
+	}
+}
+public class PushNotification implements Notification {
+
+	@Override
+	public void notifyUser()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Sending a push notification");
+	}
+}
+public class NotificationFactory {
+	public Notification createNotification(String channel)
+	{
+		if (channel == null || channel.isEmpty())
+			return null;
+		switch (channel) {
+		case "SMS":
+			return new SMSNotification();
+		case "EMAIL":
+			return new EmailNotification();
+		case "PUSH":
+			return new PushNotification();
+		default:
+			throw new IllegalArgumentException("Unknown channel "+channel);
+		}
+	}
+}
+public class NotificationService {
+	public static void main(String[] args)
+	{
+		NotificationFactory notificationFactory = new NotificationFactory();
+		Notification notification = notificationFactory.createNotification("SMS");
+		notification.notifyUser();
+	}
+}
