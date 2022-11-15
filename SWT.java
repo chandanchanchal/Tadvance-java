3073140ae962c7d526af5cecb8ecf290e53b3c62
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2022-11-15 12:10:23 +0530

    Update SWT.java

--- SWT.java
+++ SWT.java
@@ -133,3 +133,34 @@ public class BottomComposite extends Composite {
   }
 
 }
+
+
+################################################################################################
+public class MainComposite extends Composite {
+
+  /**
+   * Create the composite.
+   * @param parent
+   * @param style
+   */
+  public MainComposite(Composite parent, int style) {
+      super(parent, style);
+      setLayout(new FillLayout(SWT.HORIZONTAL));
+     
+      Composite composite = new Composite(this, SWT.NONE);
+      composite.setLayout(new GridLayout(1, false));
+     
+      TopComposite topComposite = new TopComposite(composite, SWT.BORDER);
+      topComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+     
+      BottomComposite bottomComposite = new BottomComposite(composite, SWT.BORDER);
+      bottomComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+
+  }
+
+  @Override
+  protected void checkSubclass() {
+  }
+
+}
+########################################################################################################################
