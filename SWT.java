b38088172b4b88baceb0b2626d229036c79223c6
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2022-11-15 11:17:07 +0530

    Update SWT.java

--- SWT.java
+++ SWT.java
@@ -15,3 +15,42 @@ public class HelloSWT {
       display.dispose();
   }
 }
+
+
+###################################################################################################################################
+
+public static void main(String[] args) {
+	      Display display = new Display();
+	      final Shell shell = new Shell(display);
+	      shell.setLayout(new FillLayout());
+
+	      //
+	      Composite parent = new Composite(shell, SWT.NONE);
+	     
+	      FillLayout fillLayout= new FillLayout();
+	      fillLayout.type= SWT.VERTICAL;
+	     
+	      parent.setLayout(fillLayout);
+
+	      Button b1 = new Button(parent, SWT.NONE);
+	      b1.setText("B1");
+
+	      Button b2 = new Button(parent, SWT.NONE);
+	      b2.setText("B2");
+
+	      Button button3 = new Button(parent, SWT.NONE);
+	      button3.setText("Button 3");
+	     
+	      // Windows back to natural size.
+	      shell.pack();
+	      //
+	      shell.open();
+	      while (!shell.isDisposed()) {
+	          if (!display.readAndDispatch())
+	              display.sleep();
+	      }
+	      // tear down the SWT window
+	      display.dispose();
+	  }
+
+###################################################################################################################################
