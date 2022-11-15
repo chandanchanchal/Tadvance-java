8e01c64a314404bc5e8e5fbfa1da1744d3e21110
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2022-11-15 11:46:24 +0530

    Update SWT.java

--- SWT.java
+++ SWT.java
@@ -54,3 +54,36 @@ public static void main(String[] args) {
 	  }
 
 ###################################################################################################################################
+
+public class TopComposite extends Composite {
+  private Text text;
+
+  /**
+   * Create the composite.
+   * @param parent
+   * @param style
+   */
+  public TopComposite(Composite parent, int style) {
+      super(parent, style);
+      setLayout(new FillLayout(SWT.HORIZONTAL));
+     
+      Composite composite = new Composite(this, SWT.NONE);
+      composite.setLayout(new GridLayout(1, false));
+     
+      Button btnPreferredSite = new Button(composite, SWT.CHECK);
+      btnPreferredSite.setText("Preferred Site");
+     
+      Label lblColumnWidth = new Label(composite, SWT.NONE);
+      lblColumnWidth.setText("Column width");
+     
+      text = new Text(composite, SWT.BORDER);
+      text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+  }
+
+  @Override
+  protected void checkSubclass() {
+  }
+}
+
+###################################################################################################################
