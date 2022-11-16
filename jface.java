f6dabedacf3fdc689c485480995e856571542011
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2022-11-16 10:00:21 +0530

    Update jface.java

--- jface.java
+++ jface.java
@@ -6,7 +6,6 @@ protected void createContents() {
    
    Button btnButtonU = new Button(shlResourceDemo, SWT.NONE);
    btnButtonU.setFont(SWTResourceManager.getFont("Arial", 12, SWT.NORMAL));
-   btnButtonU.setForeground(SWTResourceManager.getColor(SWT.COLOR_BLUE));
-   btnButtonU.setImage(SWTResourceManager.getImage(ResourceDemo.class, "/org/o7planning/tutorial/jface/image/check.png"));
+   btnButtonU.setForeground(SWTResourceManager.getColor(SWT.COLOR_BLUE));  
    btnButtonU.setText("Button using SWT Resource Manager");
 }
