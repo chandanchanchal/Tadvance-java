270ad82bca694ae3a86f27998d34ee9e79934932
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2022-11-22 09:39:06 +0530

    Update RCP-E4-handler.java

--- RCP-E4-handler.java
+++ RCP-E4-handler.java
@@ -9,9 +9,11 @@ public class AboutHandler {
        MessageDialog.openInformation(shell, "About", "Eclipse 4 RCP Application");
    }
 }
+
+######################################################################3
 OpenHandler.java
 
-package org.o7planning.tutorial.rcp.handler;
+
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.swt.widgets.FileDialog;
@@ -23,5 +25,47 @@ public class OpenHandler {
    public void execute(Shell shell){
        FileDialog dialog = new FileDialog(shell);
        dialog.open();
+   }
+}
+####################################################
+QuitHandler.java
+
+
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+
+public class QuitHandler {
+   @Execute
+   public void execute(IWorkbench workbench, Shell shell){
+       if (MessageDialog.openConfirm(shell, "Confirmation",
+               "Do you want to exit?")) {
+           workbench.close();
+       }
+   }
+}
+##########################################################################
+SaveHandler.java
+
+import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+
+public class SaveHandler {
+
+   @CanExecute
+   public boolean canExecute(EPartService partService) {
+       if (partService != null) {
+           return !partService.getDirtyParts().isEmpty();
+       }
+       return false;
+   }
+
+   @Execute
+   public void execute(EPartService partService) {
+       partService.saveAll(false);
    }
 }
